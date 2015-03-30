9485fa9be507a7dbf4b86818d858968485ea6a3e
Randy Gelhausen <rgelhau@gmail.com>
2015-03-30 01:55:03 -0400

    Added ISpark Scala kernel & WordCount example notebook

--- package/scripts/master_params.py
+++ package/scripts/master_params.py
@@ -14,7 +14,7 @@ pid_file = config['configurations']['ipython-env']['pid_file']
 pid_dir = '/'.join(pid_file.split('/')[0:-1])
 
 root_dir = '/home/'+user
-mkdirs=[root_dir, root_dir+'/notebooks', pid_dir, log_dir]
+mkdirs=[root_dir, root_dir+'/notebooks', pid_dir, log_dir, root_dir+'/.ipython']
 
 bash_profile = user.lower() + '/.bashrc'
 if user.lower() != 'root': bash_profile = '/home/' + bash_profile
@@ -29,5 +29,6 @@ executor_memory = str(config['configurations']['ipython-config']['executor.memor
 
 ipynb_install = 'sh ' + scripts_dir + 'ipynb_install.sh ' + root_dir + ' ' + files_dir
 start_cmd = 'source ' + bash_profile + '; sh ' + scripts_dir + 'start.sh'
-start_args = ['"'+root_dir+'/spark/bin/pyspark --master yarn-client --num-executors '+num_executors+' --executor-memory '+executor_memory+' --matplotlib"', log_dir + '/notebook.log', pid_file]
+start_args = ['"'+root_dir+'/spark/bin/pyspark --master yarn-client --num-executors '+num_executors+' --executor-memory '+executor_memory+'"', log_dir + '/notebook.log', pid_file]
+#start_args = ['"ipython notebook --profile=default --ipython-dir '+root_dir+'/.ipython --notebook-dir '+root_dir+'/notebooks"', log_dir + '/notebook.log', pid_file]
 commands = {'install': ipynb_install, 'start': start_cmd + ' ' + ' '.join(start_args)}
